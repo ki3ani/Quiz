6b66236991632b1193c39d57d5f4def67b7de2cc
Kenneth Kimani <kimanim68@gmail.com>
2023-01-12 14:20:25 +0300

    configappname

--- quiz/popquiz/views.py
+++ quiz/popquiz/views.py
@@ -1,18 +1,18 @@
 from django.http import HttpResponse
 from .models import Question, Choice
-from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 
 # Create your views here.
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('popquiz/index.html')
     context = { 'latest_question_list': latest_question_list, }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'popquiz/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'popquiz/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
